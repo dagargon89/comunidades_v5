6935f4f1c82875f6664127a93c3f1d8c22e68e17
David García González <153229290+dagargon89@users.noreply.github.com>
2025-07-23 19:40:46 -0600

    Elimina las vistas y la clase ProjecGanttView, simplificando la estructura del proyecto y eliminando funcionalidades no utilizadas. Esto contribuye a una mejor organización del código y a la reducción de la complejidad en la gestión de páginas.

--- app/Filament/Pages/ProjectGanttView.php
+++ app/Filament/Pages/ProjectGanttView.php
@@ -4,9 +4,9 @@ namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
 
-class ProjecGanttView extends Page
+class ProjectGanttView extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
-    protected static string $view = 'filament.pages.projec-gantt-view';
+    protected static string $view = 'filament.pages.project-gantt-view';
 }

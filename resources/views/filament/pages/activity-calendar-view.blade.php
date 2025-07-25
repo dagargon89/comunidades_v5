b9e2792d7aa47ab3d78cc01cc165675f57069061
dagargon89 <dagargon89@gmail.com>
2025-07-25 12:05:55 -0600

    Agrega un botón de navegación en la vista de calendario de actividades que permite regresar a la vista de Gantt de Proyectos, mejorando la experiencia del usuario al facilitar la navegación entre las dos vistas.

--- resources/views/filament/pages/activity-calendar-view.blade.php
+++ resources/views/filament/pages/activity-calendar-view.blade.php
@@ -1,4 +1,11 @@
 <x-filament-panels::page>
+    <div class="mb-4">
+        <a href="{{ url('/admin/project-gantt-view') }}">
+            <x-filament::button color="warning" icon="heroicon-o-arrow-left">
+                Regresar a Gantt de Proyectos
+            </x-filament::button>
+        </a>
+    </div>
     <form wire:submit.prevent="submit">
         {{ $this->form }}
         <div class="flex justify-end mt-4">

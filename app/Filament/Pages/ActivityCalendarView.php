c8ea92341779a9260711eb1b7d0fa70b94723a79
dagargon89 <dagargon89@gmail.com>
2025-07-25 08:41:27 -0600

    Agrega un comentario en el archivo project-gantt-view.blade.php para documentar el respaldo del archivo antes de mover la lógica de los modales. Esto mejora la claridad del código y facilita futuras modificaciones.

---
+++ app/Filament/Pages/ActivityCalendarView.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class ActivityCalendarView extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.activity-calendar-view';
+}

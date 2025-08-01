27f97aa2c0ee3d06265feb6f9eecb3b66d337443
dagargon89 <dagargon89@gmail.com>
2025-08-01 09:42:28 -0600

    Desactiva temporalmente todos los widgets en el dashboard y el panel de usuario, comentando su inclusión en las funciones correspondientes. Este cambio simplifica la interfaz y permite una revisión más clara de los elementos visuales presentados.

--- app/Filament/Usuario/Pages/Dashboard.php
+++ app/Filament/Usuario/Pages/Dashboard.php
@@ -9,19 +9,19 @@ class Dashboard extends BaseDashboard
     protected function getHeaderWidgets(): array
     {
         return [
-            \App\Filament\Usuario\Widgets\ActivityCalendarCount::class,
-            \App\Filament\Usuario\Widgets\ActivityFileStats::class,
-            \App\Filament\Usuario\Widgets\BeneficiaryStats::class,
+            // \App\Filament\Usuario\Widgets\ActivityCalendarCount::class,
+            // \App\Filament\Usuario\Widgets\ActivityFileStats::class,
+            // \App\Filament\Usuario\Widgets\BeneficiaryStats::class,
         ];
     }
 
     protected function getFooterWidgets(): array
     {
         return [
-            \App\Filament\Usuario\Widgets\ProjectActivitySummary::class,
-            \App\Filament\Usuario\Widgets\ActivityCalendarTable::class,
-            \App\Filament\Usuario\Widgets\ActivityFileTable::class,
-            \App\Filament\Usuario\Widgets\BeneficiaryRegistryTable::class,
+            // \App\Filament\Usuario\Widgets\ProjectActivitySummary::class,
+            // \App\Filament\Usuario\Widgets\ActivityCalendarTable::class,
+            // \App\Filament\Usuario\Widgets\ActivityFileTable::class,
+            // \App\Filament\Usuario\Widgets\BeneficiaryRegistryTable::class,
         ];
     }
 
